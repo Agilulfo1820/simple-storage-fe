5bc75ccce3820355bb77aeb829ebf05315803159
Dan <rusnac14@gmail.com>
2022-10-21 17:04:40 +0200

    Add Goerli to metamask

--- src/AccountInfo.js
+++ src/AccountInfo.js
@@ -1,6 +1,7 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
+import detectEthereumProvider from '@metamask/detect-provider'
 
-function AccountInfo({account, chain, provider}) {
+function AccountInfo({ account, chain, provider }) {
     const abbreviateAddress = (account) => {
         if (!account)
             return ''
@@ -8,10 +9,25 @@ function AccountInfo({account, chain, provider}) {
         return account.slice(0, 6) + '...' + account.slice(account.length - 4, account.length)
     }
 
+    const addGoerliToMetamask = async () => {
+        const provider = await detectEthereumProvider()
+
+        provider.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{chainId: '0x5'}]
+        }).then((res)=> {
+            console.log(res)
+        })
+    }
+
     return (
         <div>
             <p>Connected to {abbreviateAddress(account)}</p>
-            {chain.id === 5 || chain.id === '0x5' ? <p>Chain: {chain.name}</p> : <p>Wrong network: please connect to Goerli</p>}
+            {chain.id === 5 || chain.id === '0x5' ? <p>Chain: {chain.name}</p> :
+                <div>
+                    <h5>Wrong network: please connect to Goerli!</h5>
+                    <h6>Click <a style={{color: 'red', textDecoration: 'underline', cursor: 'pointer'}} onClick={addGoerliToMetamask}>here</a> to add Goerli to your Metamask.</h6>
+                </div>}
         </div>
     )
 }
